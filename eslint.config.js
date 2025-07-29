fe6d6f02b6a10c999caf62b2c65829c94580c220
Sukant Hajra <dev.sukant@hajra.xyz>
2025-07-29 10:01:00 -0500

    simplify eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -11,49 +11,9 @@ export default [
     },
   ]),
   {
-    files: ["**/*.ts"],
-    languageOptions: {
-      globals: {
-        document: "readonly",
-        window: "readonly",
-        console: "readonly",
-        setTimeout: "readonly",
-        clearTimeout: "readonly",
-        setInterval: "readonly",
-        clearInterval: "readonly",
-        alert: "readonly",
-        requestAnimationFrame: "readonly",
-        HTMLFormElement: "readonly",
-        HTMLInputElement: "readonly",
-        HTMLButtonElement: "readonly",
-        HTMLElement: "readonly",
-        NodeListOf: "readonly",
-        Element: "readonly",
-        HTMLAnchorElement: "readonly",
-      },
-    },
-    rules: {
-      // Keep code quality rules
-      "no-console": "off",
-    },
-  },
-  {
     files: ["**/*.html"],
     plugins: {
       html: htmlPlugin,
-    },
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        document: "readonly",
-        window: "readonly",
-        console: "readonly",
-      },
-    },
-    rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off",
     },
   },
 ];
